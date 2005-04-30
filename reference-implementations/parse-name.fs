7a616c4ddf761bb4ed7fc14f35a613a7dd710006
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2005-04-30 08:04:39 +0000

    extension-query: Votes from Alex McDonald; uncommented Voting instructions parse-word: integrated comments since 1st rfd, including the name straw poll;             shelved the rfd parse-name: successor to the PARSE-WORD rfd.

---
+++ reference-implementations/parse-name.fs
@@ -0,0 +1,25 @@
+: isspace? ( c -- f )
+    bl 1+ u< ;
+
+: isnotspace? ( c -- f )
+    isspace? 0= ;
+
+: xt-skip   ( addr1 n1 xt -- addr2 n2 ) \ gforth
+    \ skip all characters satisfying xt ( c -- f )
+    >r
+    BEGIN
+	dup
+    WHILE
+	over c@ r@ execute
+    WHILE
+	1 /string
+    REPEAT  THEN
+    r> drop ;
+
+: parse-name ( "name" -- c-addr u )
+    source >in @ /string
+    ['] isspace? xt-skip over >r
+    ['] isnotspace? xt-skip ( end-word restlen r: start-word )
+    2dup 1 min + source drop - >in !
+    drop r> tuck - ;
+
