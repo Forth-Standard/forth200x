73042e60b0b88cb49ec07062297808b93ec5bd80
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2006-05-25 10:01:37 +0000

    added ekeys

---
+++ tests/ekeys.fs
@@ -0,0 +1,22 @@
+require test/tester.fs
+
+{ k-left  -> cr ." Please press <left>" ekey }
+{ k-right -> cr ." Please press <right>" ekey }
+{ k-up    -> cr ." Please press <up>" ekey }
+{ k-down  -> cr ." Please press <down>" ekey }
+{ k-home  -> cr ." Please press <home>" ekey }
+{ k-end   -> cr ." Please press <end>" ekey }
+{ k-prior -> cr ." Please press <prior>" ekey }
+{ k-next  -> cr ." Please press <next>" ekey }
+
+\ { k1  -> cr ." Please press <F1>" ekey }
+\ { k2  -> cr ." Please press <F2>" ekey }
+\ { k3  -> cr ." Please press <F3>" ekey }
+\ { k4  -> cr ." Please press <F4>" ekey }
+\ { k5  -> cr ." Please press <F5>" ekey }
+\ { k6  -> cr ." Please press <F6>" ekey }
+\ { k7  -> cr ." Please press <F7>" ekey }
+\ { k8  -> cr ." Please press <F8>" ekey }
+\ { k9  -> cr ." Please press <F9>" ekey }
+\ { k10 -> cr ." Please press <F10>" ekey }
+
