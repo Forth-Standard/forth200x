dd1d3e4e610f6f0160fe20acae3c3b6ef18bee2d
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2006-05-28 16:32:46 +0000

    version for 2nd RfD

--- tests/ekeys.fs
+++ tests/ekeys.fs
@@ -9,14 +9,19 @@ require test/tester.fs
 { k-prior -> cr ." Please press <prior>" ekey }
 { k-next  -> cr ." Please press <next>" ekey }
 
-\ { k1  -> cr ." Please press <F1>" ekey }
-\ { k2  -> cr ." Please press <F2>" ekey }
-\ { k3  -> cr ." Please press <F3>" ekey }
-\ { k4  -> cr ." Please press <F4>" ekey }
-\ { k5  -> cr ." Please press <F5>" ekey }
-\ { k6  -> cr ." Please press <F6>" ekey }
-\ { k7  -> cr ." Please press <F7>" ekey }
-\ { k8  -> cr ." Please press <F8>" ekey }
-\ { k9  -> cr ." Please press <F9>" ekey }
-\ { k10 -> cr ." Please press <F10>" ekey }
+{ k-f1  -> cr ." Please press <F1>" ekey }
+{ k-f2  -> cr ." Please press <F2>" ekey }
+{ k-f3  -> cr ." Please press <F3>" ekey }
+{ k-f4  -> cr ." Please press <F4>" ekey }
+{ k-f5  -> cr ." Please press <F5>" ekey }
+{ k-f6  -> cr ." Please press <F6>" ekey }
+{ k-f7  -> cr ." Please press <F7>" ekey }
+{ k-f8  -> cr ." Please press <F8>" ekey }
+{ k-f9  -> cr ." Please press <F9>" ekey }
+{ k-f10 -> cr ." Please press <F10>" ekey }
+{ k-f11 -> cr ." Please press <F11>" ekey }
+{ k-f12 -> cr ." Please press <F12>" ekey }
 
+{ k-left k-shift-mask or -> cr ." Please press <shift-left>" ekey }
+{ k-left k-ctrl-mask  or -> cr ." Please press <ctrl-left>" ekey }
+{ k-left k-alt-mask   or -> cr ." Please press <alt-left>" ekey }
